d0ae060f77c6941ee5699c9c484115631ab52633
Andres <cesarandrestorresbernal604@gmail.com>
2022-11-27 17:55:43 -0500

    proyecto finalizado

--- myCard.js
+++ myCard.js
@@ -6,56 +6,79 @@ class myCard extends HTMLElement {
         this.imagen = this.getAttribute('img');
         this.icono = this.getAttribute('icono');
     }
+
     getTemplate() {
         const template = document.createElement('template');
         template.innerHTML = `
-        
         ${this.getStyles()}
         <div class="card">
-                ${this.imagen && `<div><img src="${this.imagen}"</div>`}
-                ${(this.icono && this.autor) && `<div><img class="pequeña" src="${this.icono}"><p>${this.autor}</p></div>`}
+            ${
+                this.imagen ? 
+                `<div class="card-image">
+                    <img src="${this.imagen}">
+                </div>`:''
+            }
+            ${
+                (this.icono && this.autor) ?
+                `<div class="card-user">
+                    <img src="${this.icono}">
+                    <p>
+                        ${this.autor}
+                    </p>
+                </div>`:''
+            }
+        </div>
         `
         return template;
     }
+
     getStyles() {
         const styles = `
         <style>
-        .card {
-            border-top: 5px solid var(--Red);
-            max-width: 300px;
-            margin: 5px auto;
-            border-radius: 5px;
-            padding: 5px;
-            box-shadow: 5px 5px 5px 5px grey;
-            font-size: 15px;
-        }
-        p{
-            margin-left: 45px;
-            padding: 10px;
-            align-content: justify;
-        }
-        img {
-            border-radius: 10px;
-            max-width: 206px;
-            margin: auto;
-            display: block;
-        }
-        img.pequeña{
-            position: relative;
-            top: -5px; 
-            left: 6 px;
-            border-radius: 50%;
-            width: 50px;
-            height: 50px;
-            float: left;
-            display: block;
-          }
+            .card {
+                border-top: 5px solid var(--Red);
+                width: 100%;
+                height: 300px;
+                border-radius: 10px;
+                box-shadow: 5px 5px 5px 5px rgba(0, 0, 0, .3);
+                overflow: hidden;
+                display: flex;
+                flex-direction: column;
+                align-items: stretch;
+            }
+            .card-image {
+                width: 100%;
+                height: 100%;
+                overflow: hidden;
+            }
+            .card-image img {
+                width: 100%;
+                object-fit: cover;
+                height: 100%;
+            }
+            .card-user {
+                display: flex;
+                gap: 10px;
+                align-items: center;
+                padding: 15px;
+            }
+            .card-user img {
+                border-radius: 50%;
+                width: 30px;
+                height: 30px;
+            }
+            .card-user p {
+                font-size: 15px;
+                font-family: 'Poppins';
+            }
         </style>
         `
         return styles;
     }
+
     connectedCallback() {
         this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
     }
 }
+
 customElements.define('my-card', myCard);
